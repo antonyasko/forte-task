c62d802855cf859523651dd8a0e06bc1c05a794e
Anton Yasko <yasko.anton.92@gmail.com>
2020-06-12 10:33:05 +0300

    refactor: add asset setup

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,33 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.(png|svg|jpg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name].[ext]',
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css|scss|sass)$/,
+        exclude: /\.module\.(css|scss|sass)$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+            },
+          },
+          { loader: 'sass-loader' }
+        ],
+      },
+    ],
+  },
 };
